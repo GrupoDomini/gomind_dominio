acd27f79f0823eaa7871e583180d1b6f0d2a8783
Silvio <silvio.souza@grupodomini.com>
2024-03-15 14:43:30 -0300

    fix: alterarcao baixar_relatorio_como_pdf

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="gomind_dominio",
     python_requires=">=3.6",
-    version="0.0.0",
+    version="0.0.1",
     description="GoMind dominio service",
     url="https://github.com/GrupoDomini/gomind_dominio.git",
     author="JeffersonCarvalhoGM",
